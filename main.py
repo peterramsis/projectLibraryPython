7157ae9924a0cde7daf93410edfd151b28411b4e
Peter Ramsis <peterzizo100@gmail.com>
2023-12-11 00:18:36 +0200

    finish models && add branch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 
+import pymysql
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -15,7 +16,68 @@ class Main(QMainWindow,MainUI):
         super(Main, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
+        self.db_connect()
+        self.handel_buttons()
+        self.ui_changes()
 
+
+    def ui_changes(self):
+        self.tabWidget.tabBar().setVisible(0)
+
+    def db_connect(self):
+     self.db =pymysql.Connect(db='library', user='root', password='')
+     self.cur = self.db.cursor()
+     print("Connection accepted")
+
+    def handel_buttons(self):
+        self.pushButton.clicked.connect(self.open_today_tab)
+        self.pushButton_2.clicked.connect(self.open_book_tab)
+        self.pushButton_3.clicked.connect(self.open_client_tab)
+        self.pushButton_4.clicked.connect(self.open_dashboard_tab)
+        self.pushButton_5.clicked.connect(self.open_history_tab)
+        self.pushButton_6.clicked.connect(self.open_report_tab)
+        self.pushButton_7.clicked.connect(self.open_settings_tab)
+        #add branch
+        self.pushButton_23.clicked.connect(self.add_branch)
+
+    def add_client(self):
+        pass
+
+    def add_branch(self):
+        name= self.lineEdit_36.text()
+        code= self.lineEdit_37.text()
+        location = self.lineEdit_38.text()
+
+
+        self.cur.execute(''' Insert into branch(name, code , location) Values(%s,%s,%s) ''' , (name, code ,location))
+        self.db.commit()
+        print("Branch added");
+
+    def open_today_tab(self):
+        self.tabWidget.setCurrentIndex(2)
+
+
+    def open_book_tab(self):
+       self.tabWidget.setCurrentIndex(3)
+       self.tabWidget_2.setCurrentIndex(0)
+
+    def open_client_tab(self):
+       self.tabWidget.setCurrentIndex(4)
+       self.tabWidget_3.setCurrentIndex(0)
+
+    def open_dashboard_tab(self):
+       self.tabWidget.setCurrentIndex(5)
+
+    def open_history_tab(self):
+       self.tabWidget.setCurrentIndex(6)
+
+    def open_report_tab(self):
+        self.tabWidget.setCurrentIndex(7)
+        self.tabWidget_5.setCurrentIndex(0)
+
+    def open_settings_tab(self):
+        self.tabWidget.setCurrentIndex(8)
+        self.tabWidget_4.setCurrentIndex(0)
 
 def main():
     app = QApplication(sys.argv)
--- structure.py
+++ structure.py
@@ -1,64 +1,107 @@
+import datetime
+
 from peewee import *
 db = MySQLDatabase('library', user='root', password='')
 
+
+bookStatus = ( (1, "New") , (2, "Used") , (3, 'Damaged') )
 # class BaseModel(Model):
 #     class Meta:
 #         database = db
 
 class Author(Model):
-    pass
+    name = CharField()
+    location = CharField(null=True)
+    class Meta:
+        database = db
 class Publisher(Model):
-    pass
+    name = CharField(unique=True)
+    location = CharField(null=True)
+
+    class Meta:
+        database = db
 
 class Category(Model):
-    name = CharField()
+    name = CharField(unique=True)
+    parent_category =CharField()
+
+    class Meta:
+        database = db
+
 class Book(Model):
-    title = CharField()
-    description = TextField()
-    category = ForeignKeyField(Category, backref="category")
-    code = CharField()
-    publisher = ForeignKeyField(Publisher, backref="publisher")
+    title = CharField(unique=True)
+    description = TextField(null=True)
+    category = ForeignKeyField(Category, backref="category" , null=True)
+    code = CharField(null=True)
+    barcode = CharField()
+    part_order = IntegerField(unique=True)
+    publisher = ForeignKeyField(Publisher, backref="publisher" ,null=True)
     price = DecimalField()
-    author = ForeignKeyField(Author, backref="author")
-    image = CharField()
-    status = CharField()
+    author = ForeignKeyField(Author, backref="author" ,null=True)
+    image = CharField(null=True)
+    status = CharField(choices=bookStatus)
     date = DateTimeField()
+
+    class Meta:
+        database = db
 class Client(Model):
-    name = CharField()
-    mail = CharField()
-    phone =  CharField()
-    date = DateTimeField()
-    national_id = IntegerField()
+    name = CharField(unique=True)
+    mail = CharField(null=True, unique=True)
+    phone =  CharField(null=True )
+    date = DateTimeField(default=datetime.datetime.now())
+    national_id = IntegerField(null=True,unique=True)
+
+    class Meta:
+        database = db
+
 class Branch(Model):
     name = CharField()
     code = CharField()
-    location =CharField()
+    location = CharField()
+
+    class Meta:
+        database = db
 
 class Employee(Model):
-    name = CharField()
-    mail = CharField()
+    name = CharField(unique=True)
+    mail = CharField(null=True)
     phone = CharField()
-    date = DateTimeField()
-    national_id = IntegerField()
-    permission = IntegerField()
+    date = DateTimeField(default=datetime.datetime.now())
+    national_id = IntegerField(unique=True)
+    priority = IntegerField(unique=True)
+
+    class Meta:
+        database = db
+
+
+processType = ((1, "Rent") , (2, "Retrieve"))
 
 class Daily_Movements(Model):
     book = ForeignKeyField(Book, backref="daily_book")
     client = ForeignKeyField(Client, backref="book_client")
-    type = CharField()
-    date = DateTimeField()
+    type = CharField(choices=processType)
+    date = DateTimeField(default=datetime.datetime.now())
     branch = ForeignKeyField(Branch, backref='Daily_branch')
     book_from = DateField()
     book_to  = DateField()
     employee = ForeignKeyField(Employee, backref='Daily_employee')
 
+    class Meta:
+        database = db
+
+actionType = ((1,'Login') ,(2, "Create"), (3, "Update") ,(4, "Delete") )
+
+tableChoices = ((1,'Book') ,(2, "Client"), (3, "Employee") ,(4, "Category") , (5, "Branch")  , (6, "Daily Movements") , (7, "Publisher") , (8, "Author"))
 class History(Model):
     employee = ForeignKeyField(Employee, backref='history_employee')
-    action = CharField()
-    table = CharField()
-    date = DateTimeField()
+    action = CharField(choices=actionType)
+    table = CharField(choices=tableChoices)
+    date = DateTimeField(default=datetime.datetime.now())
     branch = ForeignKeyField(Employee, backref='history_branch')
+
+    class Meta:
+        database = db
 
 
 db.connect()
-db.create_tables([Book,Client,Branch,Publisher,Author,Category,Employee])
+db.create_tables([Category,Book,Client,Branch,Publisher,Author,Employee,Daily_Movements, History,])
